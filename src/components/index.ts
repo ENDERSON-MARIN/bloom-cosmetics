fd08b027abdd7209f965ea33dc9b10d49d4e1758
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-08-02 14:50:47 -0300

    feat: add SidebarMenu component and integrate it into ShopLayout; update TopMenu and package dependencies

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 
 
-
 export * from "./products/product-grid/ProductCard";
 export * from "./products/product-grid/ProductGrid";
 export * from "./ui/not-found/PageNotFound";
-export * from "./ui/top-menu/TopMenu";
+export * from "./ui/sidebar-menu/SidebarMenu";
 export * from "./ui/title/Title";
+export * from "./ui/top-menu/TopMenu";
