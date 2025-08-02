04c0bc3cdbc3728f443d7b1942816a477c22b6e4
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-08-02 19:35:05 -0300

    feat: implement ProductPage with product details and SizeSelector

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,13 @@
-
-
 export * from "./products/product-grid/ProductCard";
 export * from "./products/product-grid/ProductGrid";
+
+
+
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/sidebar-menu/SidebarMenu";
 export * from "./ui/title/Title";
 export * from "./ui/top-menu/TopMenu";
+
+
+export * from "./product/size-selector/SizeSelector";
+export * from "./product/quantity-selector/QuantitySelector";
