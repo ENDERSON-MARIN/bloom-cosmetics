eca4c7a85ad5a67a3aeeeac0bfcf84fec94c71a4
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-08-02 18:21:05 -0300

    feat: add desing frontend CategoryPage component

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,44 @@
+import { ProductGrid, Title } from "@/components";
+import { Category, Product } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
-interface Props {
+const seedProducts = initialData.products;
+
+interface CategoryPageProps {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
-const CategoryPage = ({ params }: Props) => {
+const CategoryPage = ({ params }: CategoryPageProps) => {
   const { id } = params;
+  // Filter products based on the category id
+  const products = seedProducts.filter(
+    (product: Product) => product.gender === id
+  );
 
-  if (id === "kids") {
+  const labels: Record<Category, string> = {
+    men: "for Men",
+    women: "for Women",
+    kid: "for Kids",
+    unisex: "for everyone",
+  };
+
+  if (id !== "kid" && id !== "men" && id !== "women" && id !== "unisex") {
     notFound();
   }
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Artícles ${labels[id]}`}
+        subtitle="All our products are made with the best quality and care."
+        className="mb-2"
+      />
+
+      <ProductGrid products={products} />
+    </>
   );
 };
 
